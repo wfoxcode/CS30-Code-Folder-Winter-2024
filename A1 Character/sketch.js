a449cb143dc0feae712c7f237b92aeab30def5b6
Webster Fox <w.fox@rcsd.ca>
2024-02-29 11:50:16 -0600

    created first project

---
+++ A1 Character/sketch.js
@@ -0,0 +1,16 @@
+// A1 Character
+// Mr. Fox
+// 19 Feb 2024
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  circle(width/2, height/2, 100);
+}
