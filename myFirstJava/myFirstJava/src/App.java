b86f3a0a870ee3291c9471be649a8ce92e86c322
Webster Fox <w.fox@rcsd.ca>
2024-03-26 11:52:24 -0600

    Classes and methods demo

--- myFirstJava/myFirstJava/src/App.java
+++ myFirstJava/myFirstJava/src/App.java
@@ -2,8 +2,24 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner inp = new Scanner(System.in);
-        
-        System.out.println( (Integer.MAX_VALUE + 1) + "This is the max");
-        System.out.println((Integer.MIN_VALUE-1) + "THIS IS MIN");
+
+
+
+        String word = new String("WolfFox");
+
+        System.out.println("12" + (4 + 3));
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
