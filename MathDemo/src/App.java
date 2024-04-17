514c797f944ffdedf13f1d1148309e5b711cbf17
Webster Fox <w.fox@rcsd.ca>
2024-04-17 11:27:28 -0600

    Geometry Demo

---
+++ MathDemo/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println(1/2);
+
+        System.out.println(1.0/2);
+        System.out.println(1/2.0);
+        System.out.println(1.0/2.0);
+
+        System.out.println( (double)1/2 );
+    }
+}
